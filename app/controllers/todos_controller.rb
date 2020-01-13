32dbe51f3a13f20454446ef52d13e2dcf9a417f8
shahabranaa <shahabranaa@gmail.comssh-keygen -t rsa -C YOUR@EMAIL.com>
2020-01-13 19:05:54 +0500

    new commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -15,8 +15,34 @@ class TodosController < ApplicationController
   end
 
   def show
-  @todo = Todo.find(params[:id])
+  	@todo = Todo.find(params[:id])
   end
+
+  def edit
+  	@todo = Todo.find(params[:id])
+  end
+
+  def update
+  	@todo = Todo.create(todo_params)
+  	if @todo.update(todo_params)
+  		flash[:notice] = "Todo was updated  successfully"
+    	redirect_to todo_path(@todo)
+  	else
+    	render 'new'
+  	end
+  end
+
+  def index
+  	@todos = Todo.all
+  end
+
+  def destroy
+  @todo = Todo.find(params[:id])
+  @todo.destroy
+  flash[:notice] = "Todo was deleted successfully"
+  redirect_to todos_path
+  end
+
    
 
   private
